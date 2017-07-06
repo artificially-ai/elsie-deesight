33fd97fe5e39f79a7345f1d076e9183ede1c95dc
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-07-06 15:18:15 +0200

    Remove the MULTIPART_FORM_DATA_VALUE type.

--- src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
+++ src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
-import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @FeignClient(serviceId = "face-classifier")
 public interface ElsieDeeSightFeignClient {
 
-  @PostMapping(path = "/process", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_OCTET_STREAM_VALUE)
+  @PostMapping(path = "/process", produces = APPLICATION_OCTET_STREAM_VALUE)
   byte[] process(final @PathVariable("face") MultipartFile face);
 }
