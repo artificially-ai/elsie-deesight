0f3dc13c9f61c425b17b0683de4e7b7c97f70cd6
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-07-06 17:19:02 +0200

    Make Feign Client compatible with Multipart requests.

--- src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
+++ src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
@@ -1,15 +1,39 @@
 package ai.ekholabs.cv.client;
 
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
 
-@FeignClient(serviceId = "face-classifier")
+@FeignClient(serviceId = "face-classifier", configuration = MultipartSupportConfig.class)
 public interface ElsieDeeSightFeignClient {
 
-  @PostMapping(path = "/process", produces = APPLICATION_OCTET_STREAM_VALUE)
-  byte[] process(final @PathVariable("face") MultipartFile face);
+  @RequestMapping(
+      value = "/process",
+      method = RequestMethod.POST,
+      produces = APPLICATION_OCTET_STREAM_VALUE
+  )
+  @ResponseBody
+  byte[] process(@RequestPart MultipartFile file);
+}
+
+@Configuration
+class MultipartSupportConfig {
+
+  @Bean
+  @Primary
+  @Scope("prototype")
+  public Encoder feignFormEncoder() {
+    return new SpringFormEncoder();
+  }
 }
