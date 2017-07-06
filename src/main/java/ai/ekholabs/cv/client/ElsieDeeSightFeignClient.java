26f4af57eb33e04ea7a962be1005d4f188838636
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-07-06 14:20:19 +0200

    Add request parameter.

--- src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
+++ src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
@@ -2,6 +2,7 @@ package ai.ekholabs.cv.client;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
@@ -11,5 +12,5 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 public interface ElsieDeeSightFeignClient {
 
   @PostMapping(path = "/process", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_OCTET_STREAM_VALUE)
-  byte[] process(final MultipartFile face);
+  byte[] process(final @RequestParam(value = "face") MultipartFile face);
 }
