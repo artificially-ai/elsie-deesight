72999a9244721ee3886640b1c195d1933df7c5f5
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-07-06 11:25:39 +0200

    Add behaviour to Feign Client and Controller.

--- src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
+++ src/main/java/ai/ekholabs/cv/client/ElsieDeeSightFeignClient.java
@@ -1,8 +1,13 @@
 package ai.ekholabs.cv.client;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
 
 @FeignClient(serviceId = "elsie-deesight")
 public interface ElsieDeeSightFeignClient {
 
+  @PostMapping(path = "elsie-deesight/process", consumes = APPLICATION_OCTET_STREAM_VALUE, produces = APPLICATION_OCTET_STREAM_VALUE)
+  byte[] process(final byte[] imageBytes);
 }
--- src/main/java/ai/ekholabs/cv/controller/ElsieDeeSightController.java
+++ src/main/java/ai/ekholabs/cv/controller/ElsieDeeSightController.java
@@ -1,9 +1,36 @@
 package ai.ekholabs.cv.controller;
 
+import java.io.IOException;
+
+import ai.ekholabs.cv.client.ElsieDeeSightFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 public class ElsieDeeSightController {
 
+  private final ElsieDeeSightFeignClient elsieDeeSightClient;
 
+  @Autowired
+  public ElsieDeeSightController(final ElsieDeeSightFeignClient elsieDeeSightClient) {
+    this.elsieDeeSightClient = elsieDeeSightClient;
+  }
+
+  @PostMapping(consumes = MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<byte[]> process(final @RequestParam(value = "face") MultipartFile face) throws IOException {
+    final HttpHeaders headers = new HttpHeaders();
+    headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+
+    final byte[] resultImage = elsieDeeSightClient.process(face.getBytes());
+    return new ResponseEntity<>(resultImage, headers, HttpStatus.OK);
+  }
 }
