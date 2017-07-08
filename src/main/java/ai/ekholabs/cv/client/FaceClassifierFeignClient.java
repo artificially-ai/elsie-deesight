000b7761eee7ee3ed2b4232d697514a3a54d6c94
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-07-08 10:28:05 -0300

    Working version, do not change.

--- src/main/java/ai/ekholabs/cv/client/FaceClassifierFeignClient.java
+++ src/main/java/ai/ekholabs/cv/client/FaceClassifierFeignClient.java
@@ -7,14 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
 
 @FeignClient(serviceId = "face-classifier", configuration = MultipartSupportConfig.class)
 public interface FaceClassifierFeignClient {
@@ -22,10 +20,10 @@ public interface FaceClassifierFeignClient {
   @RequestMapping(
       value = "/process",
       method = RequestMethod.POST,
-      produces = APPLICATION_OCTET_STREAM_VALUE
+      produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
   )
   @ResponseBody
-  ResponseEntity<byte[]> process(final @RequestPart MultipartFile image);
+  byte[] process(@RequestPart MultipartFile file);
 }
 
 @Configuration
